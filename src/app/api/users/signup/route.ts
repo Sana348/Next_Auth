827d390610521a28c0ff8c90d8d819de1a160e6a
Sana Parveen <misana348@gmail.com>
2024-05-02 17:21:08 +0530

    Update signup route to await request json and add email sending functionality with hashed token in mailer
    * In `src/app/api/users/signup/route.ts`, changed `const reqBody = request.json();

--- src/app/api/users/signup/route.ts
+++ src/app/api/users/signup/route.ts
@@ -1,6 +1,52 @@
-import {connect} from '@/dbConfig/dbConfig';
-import User from '@/models/userModel';
+import { connect } from "@/dbConfig/dbConfig";
+import User from "@/models/userModel";
+import { error } from "console";
+import bcryptjs from 'bcryptjs';
+import { sendEmail } from "@/helpers/mailer";
 
-import {NextRequest, NextResponse} from 'next/server'
 
-connect()
+import { NextRequest, NextResponse } from "next/server";
+
+connect();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = request.json();
+    const { username, email, password } = reqBody;
+    //validation
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    //hash password
+    const salt = await bcryptjs.genSalt(10)
+    const hashedPassword = await bcryptjs.hash(password, salt)
+
+    const newUser = new User({
+        username,
+        email,
+        password: hashedPassword
+    })
+
+    const savedUser = await newUser.save()
+console.log(savedUser)
+
+await sendEmail({email, emailType: "VERIFY", userId: savedUser._id,} )
+
+return NextResponse.json({
+    message: "User registered successfully",
+    success : true,
+    savedUser
+})
+
+
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
